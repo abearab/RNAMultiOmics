496d5b955b73f539e28695bd3c574ad2b846ab35
abearab <abea@arcinstitute.org>
2025-01-23 00:50:40 -0800

    reorganize rmats module

--- src/multiomics/splicing/__init__.py
+++ src/multiomics/splicing/__init__.py
@@ -1,1 +1,8 @@
-from . import rmats
+## Load scripts for rmats analysis
+from .rmats import class_exon
+from .rmats import class_fromGTF
+from .rmats import extract_PSI_count
+from .rmats import get_novel_ID
+from .rmats import rmats_filtering
+from .rmats import rmats_load
+# source: https://github.com/Xinglab/rmats-turbo-tutorial/tree/main/scripts
--- src/multiomics/splicing/rmats/__init__.py
+++
@@ -1,8 +0,0 @@
-# source: https://github.com/Xinglab/rmats-turbo-tutorial/tree/main/scripts
-
-from . import class_exon
-from . import class_fromGTF
-from . import extract_PSI_count
-from . import get_novel_ID
-from . import rmats_filtering
-from . import rmats_load
