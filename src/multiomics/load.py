c65e94e6b1da7c44f78f629b80a0aa9235ad7aec
abearab <abea@arcinstitute.org>
2025-01-24 14:55:46 -0800

    update load_gtf function and fix GenomicRanges conversion

--- src/multiomics/load.py
+++ src/multiomics/load.py
@@ -2,17 +2,24 @@ import biobear as bb
 from genomicranges import GenomicRanges
 
 
-def load_gtf(GTF, type='GenomicRanges', verbose=False):
+def load_gtf(GTF, output_type='GenomicRanges', verbose=False):
     session = bb.new_session()
     gtf_df = session.read_gtf_file(GTF).to_polars()
 
     if verbose: print('GTF file loaded successfully.')
 
-    if type == 'Polars' or type == 'pl':
+    if output_type == 'Polars' or output_type == 'pl':
         return gtf_df
     
-    elif type == 'GenomicRanges' or type == 'GRanges' or type == 'gr':
-        return bb.polars_to_gr(gtf_df)
+    elif output_type == 'GenomicRanges' or output_type == 'GRanges' or output_type == 'gr':
+        # rename columns: start -> starts, end -> ends, seqname -> seqnames
+        gtf_gr = GenomicRanges.from_polars(
+            gtf_df.rename({
+                'start': 'starts', 'end': 'ends', 'seqname': 'seqnames'
+            })
+        )
+        if verbose: print('Data converted to GenomicRanges object.')
+        return gtf_gr
     
     else:
-        raise ValueError('Invalid type. Please use "Polars"/"pl" or "GenomicRanges"/"GRanges"/"gr".')
+        raise ValueError('Invalid output_type. Please use "Polars"/"pl" or "GenomicRanges"/"GRanges"/"gr".')
